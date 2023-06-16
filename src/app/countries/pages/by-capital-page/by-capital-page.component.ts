2a7c009d6958aad10488d476540e3e149ad8a864
brian24C <brianjosuecastro@hotmail.com>
2023-06-16 15:47:15 -0500

    add emitSearch to grab value of input

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCapitalPageComponent {
-
+  searchByCapital(term: string): void {
+    console.log(term);
+  }
 }
---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,25 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: [],
+})
+export class SearchBoxComponent {
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public placeholder: string = '';
+
+  emitSearch(value: string): void {
+    this.onValue.emit(value);
+  }
+}
